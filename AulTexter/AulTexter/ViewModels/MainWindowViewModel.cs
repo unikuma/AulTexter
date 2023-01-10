b8a60455d162a07c72219bceca0ea5d77c69dd8e
unikuma <85827428+unikuma@users.noreply.github.com>
2023-01-10 19:09:53 +0900

    プロジェクト作成

---
+++ AulTexter/AulTexter/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using AulTexter.Models;
+using Livet;
+using Livet.Commands;
+using Livet.EventListeners;
+using Livet.Messaging;
+using Livet.Messaging.IO;
+using Livet.Messaging.Windows;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace AulTexter.ViewModels
+{
+	public class MainWindowViewModel : ViewModel
+	{
+		// Some useful code snippets for ViewModel are defined as l*(llcom, llcomn, lvcomm, lsprop, etc...).
+		public void Initialize()
+		{
+		}
+	}
+}
