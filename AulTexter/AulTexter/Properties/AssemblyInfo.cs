623d001e5815656897cac9451bc457f2d39aecaf
unikuma <85827428+unikuma@users.noreply.github.com>
2023-01-13 22:54:07 +0900

    1.0.0

--- AulTexter/AulTexter/Properties/AssemblyInfo.cs
+++ AulTexter/AulTexter/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AulTexter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("入力した内容を基に字幕用Exoファイルを生成します。")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AulTexter")]
